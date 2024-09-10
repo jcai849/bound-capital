b2a9a27c6e19555b866cdd1d7c50715f4acea9a6
Jason Cairns <jason@cair.nz>
2024-09-10 23:43:56 +1200

    functioning with linear opcap

--- opcap.R
+++ opcap.R
@@ -1,35 +1,43 @@
 # usage: 
-# opcap determines how much needs to be saved at next pay.
-# next pay is determined relative to current date (inclusive)
-# savings: savings on day of pay (prior to receiving payment)
+# opcap determines how much needs to be saved at the outset of the current credit window.
+# current date should match the beginning of one of the supplied credits
+# savings: savings held immediately prior to outset of current credit window
 
 Credits <- function(dates) {
-	stopifnot(is.Date(dates) && length(dates))
+	stopifnot(is.Date(dates) && as.logical(length(dates)))
 	structure(dates, class=c("Credits", class(dates)))
 }
 Debit <- function(name, cost, date) {
-	stopifnot(is.character(name) && length(name) == 1)
-	stopifnot(is.Date(date) && length(date))
+	stopifnot(is.character(name) && as.logical(length(name)) == 1)
+	stopifnot(is.Date(date) && as.logical(length(date)))
 	stopifnot(is.numeric(cost) &&
 		length(cost) == 1 || length(cost) == length(date))
 	list(name=name, cost=cost, date=date) |>
-	structure(class="Debit")
+	structure(class=c("Debit", "list"))
 }
 is.Debit <- function(debit) inherits(debit, "Debit")
-Debits <- function(...) {
-	debits <- list(...)
-	stopifnot(length(debits))
-	stopifnot(all(sapply(debits, is.Debit)))
-	structure(debits, class="Debits")
+Debits <- function(x, ...) UseMethod("Debits")
+Debits.Debit <- function(x, ...) {
+	c(list(x), list(...)) |> Debits()
 }
-is.NA.Debits <- function(x) vapply(x, \(debit) length(debit$date) == 0, logical(1))
+Debits.list <- function(x, ...) {
+	stopifnot(all(sapply(x, is.Debit)))
+	structure(x, class=c("Debits", "list"))
+}
+is.empty <- function(x) UseMethod("is.empty", x)
+is.empty.Debits <- function(x) vapply(x, is.empty, logical(1))
+is.empty.Debit <- function(x) length(x$date) == 0
 Summary.Debits <- function(..., na.rm) {
-	list(...)[1] |> sapply(\(debit) .Generic(debit$cost)) |> .Generic()
+	x <- list(...)
+	if (length(x) > 1) stop("summary not implemented for more than one argument")
+	do.call(.Generic, list(x[[1]]$cost))
 }
-Ops.Debits <- function(e1, e2) {
-	if (.Generic != '-') stop("Operation not yet implemented")
-	e1[!e1$name %in% e2$name]
-}
+`%complement%` <- function(e1, e2) { 
+	filter_debits(function(e1_debit) {
+		e2_match_i <- match(e1_debit$name, e2$name)
+		if (is.na(e2_match_i)) return(TRUE)
+		! e1_debit$date %in% e2[[e2_match_i]]$date
+	}, e1) }
 head.Debits <- function(x, n=6L, ...) {
 	lapply(x, head, n) |> Debits()
 }
@@ -37,67 +45,80 @@ head.Debit <- function(x, n=6L, ...) within(x, {
 		cost <- head(cost, n)
 		date <- head(date, n)
 	})
-`$.Debits` <- function(x, name) sapply(x, `$`, name)
-`$.Debits<-` <- function(x, name, value) {
-	Map(\(x,v) {
-		x[name] <- value
-		x
-	}, x, value) |>
-	Debits()
-}
+`$.Debits` <- function(x, name) lapply(x, `[[`, name) |> do.call(c, args=_)
+`[.Debits` <- function(x, i) Debits(unclass(x)[i])
 
-main <- function(current_date, credits, debits, savings) {
-	current_future_debits  <- remove_historical_debits(current_date, debits)
+opcap <- function(current_date, credits, debits, savings) {
+	if (!current_date %in% credits) stop("current date must match the outset of supplied credit windows")
 	current_future_credits <- remove_historical_credits(current_date, credits)
-        current_debits <- remove_future_debits(current_future_credits, current_future_debits)
-	future_debits <- current_future_debits - current_debits
+	current_future_debits  <- remove_historical_debits(current_date, debits)
 
-	next_credit <- current_future_credits[1]
+	current_credit_window <- head(current_future_credits, n=2)
+        current_debits <- remove_future_debits(current_credit_window, current_future_debits)
+	future_debits <- current_future_debits %complement% current_debits
+
 	next_debits <- head(future_debits, n=1)
-	debit_savings <- save_next_debits(next_debits, next_credit, savings - sum(current_debits))
+	debit_savings <- save_next_debits(next_debits, current_credit_window, savings - sum(current_debits))
 	list(current=current_debits, future=debit_savings)
 }
 
+is.Date <- function(x) inherits(x, "Date")
+
 filter_debits <- function(filter, debits) {
 	i <- lapply(debits, filter)
-	Map(\(debit, i) { within(debit, {
+	Map(\(debit, i) within(debit, {
 		if (length(cost) > 1) cost <- cost[i]
 		date <- date[i]
-		}
-	}, debits, i)
+	}), debits, i) |>
+	Debits()
 }
 
 remove_historical_credits <- function(current_date, credits) {
-	out <- credits[credits < current_date]
-	if (!length(out)) warning("credit expired")
+	out <- credits[credits >= current_date]
+	if (!length(out)) stop("credit expired")
 	out
 }
-remove_historical_debits <- function(current_date, credits, debits) {
-	current_future_debits <- filter_debits(\(debit) item$date >= current_date, debits)
-	expired_i <- is.NA(current_future_debits)
-	lapply(current_future_debits[expired_i], \(debit) warning(sprintf("%s expired", debit$name)))
+remove_historical_debits <- function(current_date, debits) {
+	current_future_debits <- filter_debits(\(debit)
+		debit$date >= current_date, debits)
+	expired_i <- is.empty(current_future_debits)
+	lapply(current_future_debits[expired_i], \(debit)
+		warning(sprintf("%s expired", debit$name)))
 	current_future_debits[!expired_i]
 }
-remove_future_debits <- function(credits, debits) {
-	if (length(credits) < 2) error("insufficient future credits provided")
-	next_pay <- credits[1]
-	next_next_pay <- credits[2]
-	# get all items within next pay window
-	current_debits <- filter_debits(\(debit) next_pay <= debit$date & debit$date < next_next_pay, debits)
-	current_debits[!is.NA(current_debits)]
+remove_future_debits <- function(current_credit_window, debits) {
+	if (length(current_credit_window) < 2)
+		stop("insufficient future credits provided")
+	current_debits <- filter_debits(\(debit)
+		debit$date >= current_credit_window[1] &
+		debit$date < current_credit_window[2],
+		debits)
+	current_debits[!is.empty(current_debits)]
 }
 
-save_future_debits <- function(next_debits, next_credit, current_savings) {
+save_next_debits <- function(next_debits, current_credit_window, current_savings) {
 	costs <- next_debits$cost
 	dates <- next_debits$date
-	origin <- ifelse(savings_remaining == 0, current_credit,
-	           nlm(linear_origin, 1, S=current_savings, c=costs, t=dates - next_credit)$estimate)
-	amounts <- savings_ratio(next_pay + origin, costs, dates - next_pay)
-	next_debits$date <- next_credit
-	next_debits$cost <- amounts
-	next_debits
+	time_until_debits <- dates - current_credit_window[1]
+	origin <- if (current_savings == 0) { 0 } else {
+		o <- nlm(linear_origin, 30,
+		         S=current_savings, c=costs, t=time_until_debits,
+		         iterlim=1E4)
+		if (o$code > 2) stop("error locating origin") else o$estimate
+	}
+	savings_split <- savings_ratio(origin, costs, time_until_debits) *
+		as.numeric((origin + diff(current_credit_window)) / origin)
+	Map(Debit,
+	name=next_debits$name,
+	cost=savings_split,
+	date=current_credit_window[1]) |>
+	Debits()
 }
 
+# h: time since savings origin
+# S: current savings
+# c: vector of costs
+# t: time until each cost incurred
 linear_origin <- function(h, S, c, t) {
 	t <- as.integer(t)
         gr <- function(h, S, c, t)
--- test.R
+++ test.R
@@ -5,19 +5,26 @@
 # then the cost collection is returned with each cost having the correct amount to put aside for the next pay
 
 source("opcap.R")
-debug(next_savings)
 debits <- Debits(
 	Debit(name="groceries",
-	     cost=c(190, 13, 56),
-	     date=as.Date(c("2023-12-12", "2024-01-01", "2024-02-01"))),
+	     cost=c(190, 13, 56, 100),
+	     date=as.Date(c("2023-12-12", "2024-01-01", "2024-02-01", "2024-02-04"))),
 	Debit(name="electricity",
 	     cost=16,
 	     date=as.Date(c("2024-01-01", "2024-01-14", "2024-02-01"))),
-	Debit(name="event",
+	Debit(name="event1",
 	     cost=130,
-	     date=as.Date("2024-02-01")))
-
-credits <- as.Date(c("2024-01-01", "2024-01-15", "2024-02-01", "2024-02-15"))
+	     date=as.Date(c("2024-02-05", "2024-02-06"))),
+	Debit(name="event2",
+	     cost=100,
+	     date=as.Date(c("2024-02-04", "2024-02-05"))))
+credits <- Credits(as.Date(c("2023-01-01", "2024-01-01", "2024-01-15", "2024-02-01", "2024-02-05")))
 savings <- 132
+current_date <- as.Date("2024-01-01")
 
-main(as.Date("2024-01-01"), credits, debits, savings) # -> name, amount
+print("working")
+opcap(current_date, credits, debits, savings) |> str()
+
+# try when some debits expired
+print("failing")
+tryCatch(opcap(as.Date("2024-02-05"), credits, debits, savings), error=\(e) e)
